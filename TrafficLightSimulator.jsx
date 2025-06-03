6d86b814fecdca5bd7bd4a0602553794249d161c
waseem mazari <mazariwaseem110@gmail.com>
2025-06-02 23:20:18 -0700

    Added functional Component React

--- TrafficLightSimulator.jsx
+++ TrafficLightSimulator.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './Light.css'
+import { useState,useEffect } from 'react'
 function TrafficLightSimulator() {
-    
+    const [activeLight,setActiveLight]=useState('red')
+
+    useEffect(()=>{
+        const cycle=['red','yellow','green']
+        let index=0
+    },3000)
     return (
         <div>
             <h1>Traffic Light Simulator</h1>
@@ -9,7 +15,7 @@ function TrafficLightSimulator() {
                 <div className="light red">Red Light</div>
                 <div className="light yellow">Yellow Light</div>
                 <div className="light green">Green Light</div>
-
+ 
             </div>
         </div>
     )
