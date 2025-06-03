912dc5574c5a4763aaeb656eb4a04240b47e53e0
waseem mazari <mazariwaseem110@gmail.com>
2025-06-02 23:06:55 -0700

    added HTML and CSS for traffic light

---
+++ TrafficLightSimulator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Light.css'
+function TrafficLightSimulator() {
+    
+    return (
+        <div>
+            <h1>Traffic Light Simulator</h1>
+            <div className="traffic-light">
+                <div className="light red">Red Light</div>
+                <div className="light yellow">Yellow Light</div>
+                <div className="light green">Green Light</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default TrafficLightSimulator
